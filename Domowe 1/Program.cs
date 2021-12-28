d057762b02d34785b95c9b318c6a09973858db14
bezglowy <marcinkowol@interia.pl>
2021-12-28 23:33:56 +0100

    Add all tasks.

--- Domowe 1/Program.cs
+++ Domowe 1/Program.cs
@@ -18,7 +18,7 @@ namespace Domowe_1
 
 
             Console.WriteLine("Task 2:");
-            Console.WriteLine("Podaj pierwszą ltierę: ");
+            Console.WriteLine("Podaj pierwszą literę: ");
             char variable1 = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("Podaj drugą literę: ");
             char variable2 = Convert.ToChar(Console.ReadLine());
@@ -27,6 +27,71 @@ namespace Domowe_1
             Console.WriteLine($"Kolejność podana przez użytkownika {variable1}, {variable2}, {variable3} \r\n W odwrotnej kolejności: {variable3}, {variable2}, {variable1}");
 
             Console.WriteLine("Task 3:");
+            Console.WriteLine("Podaj szerokość prostokąta: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj długość prostokąta: ");
+            double height = double.Parse(Console.ReadLine());
+            double diagonal = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+            Console.WriteLine($"Przekątna prostokąta wynosi: {diagonal}");
+
+            Console.WriteLine("Task 4:");
+            float firstNumberVariable;
+            float secondNumberVariable;
+            string stringVariable;
+            int properFirstNumberVariable = 10;
+            float properSecondNumberVariable = 12.5f;
+            string properStringVariable = "Szkoła Dotneta";
+
+            Console.WriteLine("Wprowadź pierwszą zmienną liczbową: ");
+            firstNumberVariable = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadź drugą zmienną liczbową: ");
+            secondNumberVariable = float.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadź zmienną tekstową: ");
+            stringVariable = Console.ReadLine();
+            Console.WriteLine("Dane wprowadzone przez użytkownika: ");
+            Console.WriteLine($"a: {firstNumberVariable} \r\n b: {stringVariable} \r\n c: {secondNumberVariable}");
+            Console.WriteLine("Dane prawidłowe: ");
+            Console.WriteLine($"a: {properFirstNumberVariable} \r\n b: {properStringVariable} \r\n c: {properSecondNumberVariable}");
+            if (firstNumberVariable == properFirstNumberVariable && secondNumberVariable == properSecondNumberVariable && stringVariable == properStringVariable)
+            {
+                Console.WriteLine("Brawo! Udało Ci się wpisać prawidłowe dane!");
+
+            }
+            else
+            {
+                Console.WriteLine("Buuu... :( Dane nieprawidłowe! Spróbuj ponownie!");
+            }
+
+            Console.WriteLine("Task 5:");
+            User user1 = new User();
+            Console.WriteLine("Podaj imię: ");
+            user1.Name = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko: ");
+            user1.Surname = Console.ReadLine();
+            Console.WriteLine("Podaj wiek: ");
+            user1.Age = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj płeć (m - mężczyzna, k - kobieta): ");
+            user1.Sex = char.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj numer telefonu: ");
+            user1.PhoneNumber = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj adres e-mail: ");
+            user1.Email = Console.ReadLine();
+            Console.WriteLine("Podaj przypisany numer użytkownika: ");
+            user1.UserNumber = ulong.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swoją wagę: ");
+            user1.Weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadź numer ubezpiecznenia: ");
+            user1.Id = ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine("Witaj użytkowniku! \r\n Imię: " + user1.Name + " \r\n Nazwisko: " + user1.Surname + "\r\n Wiek: " + user1.Age + "\r\n Płeć: " + user1.Sex);
+            Console.WriteLine($"Numer telefonu: {user1.PhoneNumber} \r\n Numer użytkownika: {user1.UserNumber} \r\n Waga: {user1.Weight} \r\n Numer ubezpieczenia: {user1.Id} ");
+
+
+
+
+
+
+
 
 
         }
---
+++ Domowe 1/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domowe_1
+{
+    public class User
+    {
+        private string name;
+        private string surname;
+        private byte age;
+        private char sex;
+        private ulong id;
+        private ulong userNumber;
+        private string email;
+        private float weight;
+        private uint phoneNumber;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Surname { get => surname; set => surname = value; }
+        public byte Age
+        {
+            get => age;
+            set
+            {
+                if ((value > 1) && (value < 150))
+                {
+                    age = value;
+                }
+            }
+        }
+        public char Sex
+        {
+            get => sex;
+            set
+            {
+                if ((value == 'm') | (value == 'k'))
+                {
+                    sex = value;
+                }
+                else
+                {
+                    sex = 'u';
+                }
+            }
+
+        }
+
+        public ulong Id
+        {
+            get => id;
+            set => id=value;
+        }
+
+        public ulong UserNumber { get => userNumber; set => userNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public float Weight { get => weight; set => weight = value; }
+        public uint PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+    }
+}
+
