52ece76fcbe696f07ae1cdfed0ba2eb566b3c795
bezglowy <marcinkowol@interia.pl>
2021-12-30 15:50:45 +0100

    Week2, Lesson 7

---
+++ Kalkulator/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2Calculator
+{
+    public class Calculator
+    {
+        public static float AddNumbers(float firstNumber, float secondNumber)
+        {
+            float score = firstNumber + secondNumber;
+            return score;
+        }
+        public static float SubtractNumbers(float firstNumber, float secondNumber)
+        {
+            float score = firstNumber - secondNumber;
+            return score;
+        }
+        public static float DivideNumbers(float firstNumber, float secondNumber)
+        {
+                float score = firstNumber / secondNumber;
+                return score;
+
+        }
+        public static float MultiplyNumbers(float firstNumber, float secondNumber)
+        {
+            float score = firstNumber * secondNumber;
+            return score;
+        }
+
+    }
+}
---
+++ Kalkulator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w prostym kalkulatorze!");
+
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            float firstNumber = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            float secondNumber = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj numer operacji do wykonania: \r\n 1. Dodawania \r\n 2. Odejmowanie \r\n 3. Mnożenie \r\n 4. Dzielenie");
+            int selectedOperation = int.Parse(Console.ReadLine());
+
+            if(selectedOperation  == 1)
+            {
+                Console.WriteLine($"Wybrano operację dodawania liczb {firstNumber} i {secondNumber}");
+                Console.WriteLine($"Wynik to {Homework2Calculator.Calculator.AddNumbers(firstNumber, secondNumber)}");
+            }
+            else if(selectedOperation ==2)
+            {
+                Console.WriteLine($"Wybrano operację odejmowania liczb {firstNumber} i {secondNumber}");
+                Console.WriteLine($"Wynik to {Homework2Calculator.Calculator.SubtractNumbers(firstNumber, secondNumber)}");
+            }
+            else if (selectedOperation == 3)
+            {
+                Console.WriteLine($"Wybrano operację mnożenia liczb {firstNumber} i {secondNumber}");
+                Console.WriteLine($"Wynik to {Homework2Calculator.Calculator.MultiplyNumbers(firstNumber, secondNumber)}");
+            }
+            else if (selectedOperation == 4)
+            {
+                if (secondNumber != 0)
+                {
+                    Console.WriteLine($"Wybrano operację dzielenia liczb {firstNumber} i {secondNumber}");
+                    Console.WriteLine($"Wynik to {Homework2Calculator.Calculator.DivideNumbers(firstNumber, secondNumber)}");
+                }
+                else
+                {
+                    Console.WriteLine("Nie można dzielić przez 0");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Wybrano nieprawidłową operację");
+            }
+
+
+        }
+    }
+}
